4a336381b99caa610f648fe190275c0b1f7af5c5
Alex Wintle <alexwintle663@gmail.com>
2025-07-13 18:41:43 +0100

    temp: commit

--- src/__tests__/integration/actuatorRouter.test.ts
+++ src/__tests__/integration/actuatorRouter.test.ts
@@ -1,16 +1,7 @@
 import request from 'supertest';
 import app from '../../app';
-import { closeConnection, connectToDatabase } from '../../config/mongoClient';
 
 describe('GET responses', () => {
-
-  beforeAll(async () => {
-    await connectToDatabase();
-  });
-
-  afterAll(async () => {
-    await closeConnection()
-  })
 
   test('Should list actuator endpoints when GET /actuator/list', async () => {
     const response = await request(app).get('/actuator/list');
@@ -19,15 +10,6 @@ describe('GET responses', () => {
     expect(response.body).toHaveProperty('health');
     expect(response.body).toHaveProperty('info');
     expect(response.body).toHaveProperty('metrics');
-  });
-
-  test('Should list service health when GET /actuator/health', async () => {
-    const response = await request(app).get('/actuator/health');
-
-    expect(response.status).toBe(200);
-    expect(response.body).toHaveProperty('statusCode', 200);
-    expect(response.body).toHaveProperty('components.app.status', 'UP');
-    expect(response.body).toHaveProperty('components.mongo.status', 'UP');
   });
 
 });
--- src/config/mongoClient.ts
+++ src/config/mongoClient.ts
@@ -4,15 +4,13 @@ const connectionString = process.env.MONGO_URI || 'mongodb://localhost:27017';
 const client = new MongoClient(connectionString);
 
 let db: Db | null = null;
-let isConnected = false;
 
 export const connectToDatabase = async (): Promise<Db> => {
-  if (isConnected && db) return db;
+  if (db) return db;
 
   try {
     await client.connect();
     db = client.db('recipme');
-    isConnected = true;
     console.log("Connected to MongoDB");
     return db;
   } catch (error) {
@@ -22,13 +20,8 @@ export const connectToDatabase = async (): Promise<Db> => {
 };
 
 export const closeConnection = async (): Promise<void> => {
-  if (isConnected) {
-    console.log("Closing MongoDB connection...");
-    await client.close();
-    isConnected = false;
-    db = null;
-    console.log("MongoDB connection closed");
-  } else {
-    console.log("MongoDB connection already closed or not yet established.");
-  }
+  console.log("Closing MongoDB connection...");
+  await client.close();
+  db = null;
+  console.log("MongoDB connection closed");
 };
