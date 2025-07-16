822c64e906b00466927ecb430c8fd8ad6bd667f7
Alex Wintle <alexwintle663@gmail.com>
2025-07-16 23:44:58 +0100

    feat: integration test for creating and finding user

--- src/__tests__/integration/repository/usersRepository.test.ts
+++ src/__tests__/integration/repository/usersRepository.test.ts
@@ -1,10 +1,15 @@
+import { Collection } from "mongodb";
+import { accessCollection } from "../../../config/mongoClient";
 import { User } from "../../../model/User";
 import { findUserByUid, saveUser } from "../../../repository/usersRepository";
 import { startTestDatabase, stopTestDatabase } from "../../utils/testDatabase"
 
 describe('usersRepository', () => {
+  let usersCollection: Collection<User>;
+
   beforeAll(async () => {
     await startTestDatabase();
+    usersCollection = await accessCollection<User>(process.env.USER_COLLECTION_NAME ?? "users");
   });
 
   afterAll(async () => {
@@ -12,20 +17,38 @@ describe('usersRepository', () => {
   })
 
   test('Should save a user to the users repository', async () => {
-    const mockUser: User = {
+    const newUser: User = {
       username: 'johnsmith',
       uid: 'abc123',
       status: 'active',
       createdAt: new Date().toISOString(),
     };
 
-    const insertedId = await saveUser(mockUser);
+    const insertedId = await saveUser(newUser);
     expect(insertedId).toBeDefined();
 
-    const foundUser = await findUserByUid(mockUser.uid);
-    expect(foundUser).not.toBeNull();
-    expect(foundUser?.uid).toBe(mockUser.uid);
-    expect(foundUser?.username).toBe(mockUser.username);
+    const foundUser = await usersCollection.findOne({ uid: newUser.uid })
 
+    expect(foundUser).not.toBeNull();
+    expect(foundUser?.uid).toBe(newUser.uid);
+    expect(foundUser?.username).toBe(newUser.username);
   })
-})
+
+  test('Should find a saved user in the users repository', async () => {
+    const savedUser: User = {
+      username: 'johnsmith',
+      uid: 'abc123',
+      status: 'active',
+      createdAt: new Date().toISOString(),
+    };
+
+    await usersCollection.insertOne(savedUser);
+
+    const foundUser = await findUserByUid(savedUser.uid);
+
+    expect(foundUser).not.toBeNull();
+    expect(foundUser?.username).toBe(savedUser.username);
+    expect(foundUser?.uid).toBe(savedUser.uid);
+    expect(foundUser?.status).toBe(savedUser.status);
+  })
+});
--- src/repository/usersRepository.ts
+++ src/repository/usersRepository.ts
@@ -2,14 +2,19 @@ import { ObjectId } from "mongodb";
 import { accessCollection } from "../config/mongoClient"
 import { User } from "../model/User";
 
-const usersCollection = await accessCollection<User>(process.env.USER_COLLECTION_NAME ?? "users")
+const getUsersCollection = async () => {
+  return await accessCollection<User>(process.env.USER_COLLECTION_NAME ?? "users");
+};
 
 export const saveUser = async (newUser: User): Promise<ObjectId> => {
-  const result = await usersCollection.insertOne(newUser)
+  const usersCollection = await getUsersCollection();
+  const result = await usersCollection.insertOne(newUser);
 
-  return result.insertedId
+  return result.insertedId;
 }
 
 export const findUserByUid = async (uid: string): Promise<User | null> => {
+  const usersCollection = await getUsersCollection();
+
   return await usersCollection.findOne({ uid });
 }
